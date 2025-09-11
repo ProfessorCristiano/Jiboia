29c471b04d0959a98977bf629fff3d44efbdd83f
Cristiano Teixeira <prof.cristiano.info@gmail.com>
2025-09-11 19:22:47 -0300

    Update Maior-que-10.py

--- Exemplos/Maior-que-10.py
+++ Exemplos/Maior-que-10.py
@@ -1,8 +1,17 @@
 from jiboia import *
 
-x=leia("digite um valor para numero 1: ")
-se(inteiro(x) > 10,
-   executar("escreva(x, 'é maior que 10')"),
-   executar("escreva(x, 'não é maior do que 10')")
 
-  )
+x = leia("Digite um valor para número 1: ")
+
+se(inteiro(x) > 10,
+   seVerdadeiro(
+       lambda: escreva('Valor informado:'),
+       lambda: escreva(x),
+       lambda: pulalinha(),
+       lambda: escreva('é maior que 10')
+   ),
+   seFalso(
+       lambda: escreva(x),
+       lambda: escreva('não é maior do que 10')
+   )
+)
